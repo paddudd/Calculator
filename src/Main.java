59ad7e722ae48695c38746f704789b77c27d1d97
paddudd <albinlehtipalo@gmail.com>
2025-11-08 14:45:27 +0100

    cleaned up

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Select a calculation type:");
@@ -16,35 +20,39 @@ public class Main {
 
         if (value > 4 || value < 1) {
             System.out.println("FUCK OFF CUNT!!!!");
-            System.exit(0);
         }
         else {
 
-        System.out.print("Enter the first number for your calculation: ");
-        int x = input.nextInt();
-        System.out.print("Enter the second number for your calculation: ");
-        int y = input.nextInt();
+            System.out.print("Enter the first number for your calculation: ");
+            double x = input.nextDouble();
+            System.out.print("Enter the second number for your calculation: ");
+            double y = input.nextDouble();
 
             if (value == 1) {
-            int sum = x + y;
-            System.out.printf("The sum is: %d%n", sum);
-        }
-            if (value == 2) {
-                int difference = x - y;
-                System.out.printf("The difference is: %d%n", difference);
-            }
-
-            if (value == 3) {
-                int product = x * y;
-                System.out.printf("The product is: %d%n", product);
-            }
-
-            if (value == 4) {
-                int quotient = x / y;
-                System.out.printf("The quotient is: %d%n", quotient);
-            }
+                System.out.println("The sum is: " + addition(x, y));
+            } else if (value == 2) {
+                System.out.println("The difference is: " + subtraction(x, y));
+            } else if (value == 3) {
+                System.out.println("The product is: " + multiplication(x, y));
+            } else System.out.println("The quotient is: " + division(x, y));
 
         }
+    }
+
+    public static double addition(double x, double y) {
+        return x + y;
+    }
+
+    public static double subtraction(double x, double y) {
+        return x - y;
+    }
+
+    public static double multiplication(double x, double y) {
+        return x * y;
+    }
+
+    public static double division(double x, double y) {
+        return x / y;
     }
 }
 
