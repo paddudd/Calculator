867102218af926b245184f45b676c7759808f8ac
Shahi Islam <mohammed.islam@itv.com>
2025-11-13 12:32:20 +0000

    Improvements

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,24 @@ import java.util.Scanner;
 public class Main {
 
     public static double memory;
-    public static int count;
+    public static int count = 0;
 
     public static void main(String[] args) {
         boolean loop = true;
-        boolean newLoop = false;
         Scanner input = new Scanner(System.in);
 
-        while (loop) {
-            if (count == 0 || newLoop) {
-                start();
-                newLoop = false;
-
-            } else {
-                System.out.print("Do you wish to perform another calculation on " + memory + "? [Y]/[N]");
-                String answerContinue = input.nextLine();
-                if (answerContinue.equalsIgnoreCase("Y")) {
-                    anotherCalc();
-
-                } else {
-                    System.out.print("Do you wish to continue calculating? [Y]/[N]");
-                    String answer = input.nextLine();
-                    if (!answer.equalsIgnoreCase("Y")) {
-                        System.out.println("Byebye");
-                        loop = false;
-
-                    } else {
-                        newLoop = true;
-                    }
-                }
+        while (loop || count == 0) {
+            start(input);
+            System.out.print("Do you wish to perform another calculation on " + memory + "? [Y]/[N]");
+            String answerContinue = input.nextLine();
+            if (answerContinue.equalsIgnoreCase("N")) {
+                System.out.println("Byebye");
+                loop = false;
             }
         }
-
     }
 
-    public static void start() {
-        Scanner input = new Scanner(System.in);
-
+    public static void start(Scanner input) {
         System.out.println("Select a calculation type:");
         System.out.println("[1] Addition");
         System.out.println("[2] Subtraction");
@@ -53,85 +34,41 @@ public class Main {
         if (value > 5 || value < 1) {
             System.out.println("Invalid input");
         } else {
+            double x, y;
 
-            System.out.print("Enter the first number for your calculation: ");
-            double x = input.nextDouble();
-            System.out.print("Enter the second number for your calculation: ");
-            double y = input.nextDouble();
+            if (count == 0) {
+                System.out.print("Enter the first number for your calculation: ");
+                x = input.nextDouble();
+                System.out.print("Enter the second number for your calculation: ");
+            } else {
+                System.out.print("Enter the number you wish to calculate with on " + memory + ": ");
+                x = memory;
+            }
+            y = input.nextDouble();
 
             if (value == 1) {
-                System.out.println("The sum is: " + addition(x, y));
                 memory = addition(x, y);
-            } else if (value == 2) {
-                System.out.println("The difference is: " + subtraction(x, y));
-                memory = subtraction(x, y);
-            } else if (value == 3) {
-                System.out.println("The product is: " + multiplication(x, y));
-                memory = multiplication(x, y);
-            } else if (value == 4) {
-                System.out.println("The quotient is: " + division(x, y));
-                memory = division(x, y);
-
-            } else {
-                System.out.println("The remainder of the division is: " + modulus(x, y));
-                memory = modulus(x, y);
-            }
-            count++;
-            System.out.println("Current amount of calculations this session: " + count);
-        }
-    }
-
-    public static void anotherCalc() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Select a calculation type:");
-        System.out.println("[1] Addition");
-        System.out.println("[2] Subtraction");
-        System.out.println("[3] Multiplication");
-        System.out.println("[4] Division");
-        System.out.println("[5] Modulus");
-
-        System.out.print("Enter selected calculation: ");
-        int value = input.nextInt();
-
-        if (value > 5 || value < 1) {
-            System.out.println("Invalid input");
-        } else {
-
-            System.out.print("Enter the number you wish to calculate with on " + memory + ": ");
-            double z = input.nextDouble();
-
-            if (value == 1) {
-                double temp = memory;
-                memory = addition(z, temp);
                 System.out.println("The sum is: " + memory);
-
             } else if (value == 2) {
-                double temp = memory;
-                memory = subtraction(temp, z);
+                memory = subtraction(x, y);
                 System.out.println("The difference is: " + memory);
 
             } else if (value == 3) {
-                double temp = memory;
-                memory = multiplication(z, temp);
+                memory = multiplication(y, x);
                 System.out.println("The product is: " + memory);
 
             } else if (value == 4) {
-                double temp = memory;
-                memory = division(temp, z);
+                memory = division(x, y);
                 System.out.println("The quotient is: " + memory);
 
             } else {
-                double temp = memory;
-                memory = modulus(z, temp);
+                memory = modulus(y, x);
                 System.out.println("The remainder of the division is: " + memory);
             }
-
             count++;
             System.out.println("Current amount of calculations this session: " + count);
         }
     }
-
 
     public static double addition(double x, double y) {
         return x + y;
